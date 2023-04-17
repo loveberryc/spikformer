ebb5518eb457e98269c6e0e75df6a8e9595a593d
Kaiyu <82535927+loveberryc@users.noreply.github.com>
2023-04-17 17:16:57 +0800

    Update train.py
    wandb.login(key='faa5adf85471403e0a69a6b4548d56b45ec155ae')

--- cifar10/train.py
+++ cifar10/train.py
@@ -324,7 +324,7 @@ def main():
 
     if args.log_wandb:
         if has_wandb:
-            wandb.Api(api_key='faa5adf85471403e0a69a6b4548d56b45ec155ae')
+            wandb.login(key='faa5adf85471403e0a69a6b4548d56b45ec155ae')
             wandb.init(project=args.experiment, config=args)
         else:
             _logger.warning("You've requested to log metrics to wandb but package not found. "
